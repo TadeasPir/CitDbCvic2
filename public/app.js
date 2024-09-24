491fb11f68703b285b9b29b9a78aa66c7a839276
Tadeáš Pirich <tada.pirich@gmail.com>
2024-09-24 12:41:35 +0200

    azsxdcfgvbhjn

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 
 // Fetch and display posts
 function fetchPosts() {
-    fetch('/routes/posts.jsss')
+    fetch('/routes/posts.js')
         .then(response => response.json())
         .then(posts => {
             const postsDiv = document.getElementById('posts');
